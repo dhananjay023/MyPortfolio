a6cbaf68eb9561509a47849c26113095ed6e9824
Dhananjay Varshney <160857645+dhananjay023@users.noreply.github.com>
2024-06-14 00:24:13 +0530

    Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/development.svg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "a man using a computer";
 
 /**
  * Project list
@@ -31,28 +31,22 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
-    description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
-  },
-  {
     title: "Web Development for Beginners",
     description:
       "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
     url: "https://github.com/microsoft/web-dev-for-beginners",
   },
   {
-    title: "My Resume Site",
+    title: "My Resume",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "The resume repository contains the HTML and CSS code. The goal is to create a clean and web-based resume that highlights my skills, experience, and education.",
+    url: "https://github.com/dhananjay023/Resume",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "My Portfolio",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "The portfolio repository contains the code for my online portfolio, built with JavaScript, React, HTML, and CSS.",
+    url: "https://dhananjay023.github.io/myportfolio",
   },
 ];
 
